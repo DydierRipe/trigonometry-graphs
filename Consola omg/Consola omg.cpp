df72e5648545f0e0af58e70e9aeb1828489d650f
Dydier Ripe <dydierripe@gmail.com>
2021-10-23 22:48:01 -0500

    Some variables now change according to the original parameters.(Vectores)
    Graphics fixed, now the functions graphics shows better and the fuctions now are not upside down.

--- Consola omg/Consola omg.cpp
+++ Consola omg/Consola omg.cpp
@@ -11,6 +11,8 @@
 
 using namespace std;
 
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820
+
 struct Coords
 {
 	int X;
@@ -61,7 +63,7 @@ public:
 	PlanoCartesiano(Coords Cor1, Coords Cor2, Coords CP1, Coords CP2); // another constructor is needed
 	void CRectangle(Coords Cor1, Coords Cor2); // create the perimeter
 	void eje(Coords Cor1, Coords Cor2); // create the axis and the measures
-	void Vectores(Coords CP1, Coords CP2); // Graph the vectors and get their stats
+	void Vectores(Coords CP1, Coords CP2, Coords C1, Coords C2); // Graph the vectors and get their stats
 	void NoSe(Coords CP1, Coords CP2, Coords P0, double A1, double A2); // I dont know
 	// is a joke, adapt the arc radius acording to the vectors distance (needs to be fixed)
 
@@ -74,7 +76,7 @@ public:
 	void Function(Coords CP1, Coords CP2, float iterator, int ID);
 
 	double CalDist(Coords CP1, Coords CP2); // calcule the distance between tho vectors
-	double CalAng(Coords CP1, int x, int y, bool Ft); // cancule the angle between twho vectors acording to the point 0
+	double CalAng(Coords CP1, int x, int y, bool Ft); // cancule the angle between two vectors acording to the point 0
 }; 
 
 PlanoCartesiano::PlanoCartesiano(Coords Cor1, Coords Cor2, Coords CP1, Coords CP2)
@@ -85,11 +87,12 @@ PlanoCartesiano::PlanoCartesiano(Coords Cor1, Coords Cor2, Coords CP1, Coords CP
 	PlanoCartesiano::eje(Cor1, Cor2);
 	Centro.X = (Cor2.X / 2) + (Cor1.X / 2);
 	Centro.Y = (Cor2.Y / 2) + (Cor1.Y / 2);
-	//Vectores(CP1, CP2);
+	Vectores(CP1, CP2, Cor1, Cor2);
 
-	//arc(Centro.X, Centro.Y, 0, 360, 100); 
+	//arc(Centro.X - 110, Centro.Y - 110, 0, 360, 100); 
+	//oval(Centro.X, Centro.Y, 0, 360, 100, 50);
 
-	// Function(Cor1, Cor2, 10, someNumber);
+	//Function(Cor1, Cor2, 10, 2);
 }
 
 void PlanoCartesiano::CRectangle(Coords Cor1, Coords Cor2)
@@ -117,13 +120,13 @@ void PlanoCartesiano::eje(Coords Cor1, Coords Cor2)
 	}
 }
 
-void PlanoCartesiano::Vectores(Coords CP1, Coords CP2)
+void PlanoCartesiano::Vectores(Coords CP1, Coords CP2, Coords C1, Coords C2)
 {
 	// graph the vectors
-	line(Centro.X, Centro.Y, CP1.X * 10 + 405, CP1.Y * -10 + 305);
-	line(Centro.X, Centro.Y, CP2.X * 10 + 405, CP2.Y * -10 + 305);
-	circle(CP1.X * 10 + 405, CP1.Y * -10 + 305, 10);
-	circle(CP2.X * 10 + 405, CP2.Y * -10 + 305, 10);
+	line(Centro.X, Centro.Y, CP1.X * 10 + ((C2.X / 2) + 10), CP1.Y * -10 + ((C2.Y / 2) + 10));  // get the chart coords and divides it on 2 and sum 10
+	line(Centro.X, Centro.Y, CP2.X * 10 + ((C2.X / 2) + 10), CP2.Y * -10 + ((C2.Y / 2) + 10));
+	circle(CP1.X * 10 + ((C2.X / 2) + 10), CP1.Y * -10 + ((C2.Y / 2) + 10), 10);
+	circle(CP2.X * 10 + ((C2.X / 2) + 10), CP2.Y * -10 + ((C2.Y / 2) + 10), 10);
 
 	Coords P0; P0.X = 0; P0.Y = 0;
 
@@ -147,12 +150,12 @@ void PlanoCartesiano::Vectores(Coords CP1, Coords CP2)
 		double Cos2 = (sqrt((pow(CP1.X, 2) + pow(CP1.Y, 2))) * sqrt((pow(CP2.X, 2) + pow(CP2.Y, 2))));
 		double Cos = Cos1 / Cos2;
 		
-		double Res = acos(Cos) * 180 / 3.141592653589793;
+		double Res = acos(Cos) * 180 / PI;
 
 		// stats
 		string CoordsP1 = "P1: " + std::to_string(CP1.X) + ", " + std::to_string(CP1.Y);
 		string CoordsP2 = "       P2: " + std::to_string(CP2.X) + ", " + std::to_string(CP2.Y);
-		string Angle = "       Angle: " + std::to_string(Res);
+		string Angle = "       Angle: " + std::to_string(XLineas);
 		string Dis = "       Distance: " + std::to_string(CalDist(CP2, P0));
 
 		outtextxy(10, 650, CoordsP1);
@@ -221,7 +224,7 @@ double PlanoCartesiano::CalAng(Coords CP1, int x, int y, bool Ft)
 	double Cos2 = (sqrt((pow(CP1.X, 2) + pow(CP1.Y, 2))) * sqrt((pow(11, 2) + pow(0, 2))));
 	double Cos = Cos1 / Cos2;
 
-	double Res = acos(Cos) * 180 / 3.141592653589793;
+	double Res = acos(Cos) * 180 / PI;
 
 	if (((CP1.X > 0 && CP1.Y < 0) || (CP1.X < 0 && CP1.Y < 0)) && Ft)
 	{
@@ -233,60 +236,60 @@ double PlanoCartesiano::CalAng(Coords CP1, int x, int y, bool Ft)
 
 void PlanoCartesiano::Function(Coords CP1, Coords CP2, float iterator, int ID)
 {
-	
-	double Pos[1000]; // save each result 
-	float Conti = -380; int J = 0; // this wrong
 
-	for (int i = -38; i <= 39; i++)
+	double Pos[1000]; // save each result 
+	float Conti = XLineas * -10 + CP1.X; int J = 0; // this wrong
+
+	for (int i = XLineas / -2; i <= XLineas / 2; i++)
 	{
 		switch (ID)
 		{
-			case 1:
-				Pos[J] = sin(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 1:
+			Pos[J] = sin(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 91:
-				Pos[J] = cos(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 91:
+			Pos[J] = cos(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 12:
-				Pos[J] = (sin(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 12:
+			Pos[J] = (sin(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 
-			case 912:
-				Pos[J] = (cos(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 912:
+			Pos[J] = (cos(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 
-			case 2:
-				Pos[J] = tan(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 2:
+			Pos[J] = tan(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 92:
-				Pos[J] = cotan(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 92:
+			Pos[J] = cotan(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 22:
-				Pos[J] = (tan(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 22:
+			Pos[J] = (tan(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 
-			case 922:
-				Pos[J] = (cotan(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 922:
+			Pos[J] = (cotan(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 
-			case 3:
-				Pos[J] = sec(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 3:
+			Pos[J] = sec(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 93:
-				Pos[J] = csc(Conti * 3.141592653589793 / 180); // calcule all on radians
+		case 93:
+			Pos[J] = csc(Conti * PI / 180); // calcule all on radians
 			break;
 
-			case 32:
-				Pos[J] = (sec(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 32:
+			Pos[J] = (sec(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 
-			case 932:
-				Pos[J] = (csc(Conti * 3.141592653589793 / 180)) / (Conti * 3.141592653589793 / 180);
+		case 932:
+			Pos[J] = (csc(Conti * PI / 180)) / (Conti * PI / 180);
 			break;
 		}
 
@@ -295,10 +298,11 @@ void PlanoCartesiano::Function(Coords CP1, Coords CP2, float iterator, int ID)
 	}
 
 	J = 0;
-	for (float i = CP1.X; i < CP2.X; i+=10)
+	for (float i = CP1.X; i < CP2.X; i += 10)
 	{
-		line(i, ((CP2.Y / 2) + (CP1.Y / 2)) + (Pos[J] * 100), i + 10, ((CP2.Y / 2) + (CP1.Y / 2)) + (Pos[J + 1] * 100));
-		//circle(i, (CP2.Y / 2) + (Pos[J] * 100), 10); // a graph with each point
+		
+		//line(i, ((CP2.Y / 2) + (CP1.Y / 2)) + (Pos[J] * -100), i + 10, ((CP2.Y / 2) + (CP1.Y / 2)) + (Pos[J + 1] * -100));
+		circle(i, ((CP2.Y - CP1.Y) / 2) + (Pos[J] * -100), 10); // a graph with each point
 		J++;
 	}
 }
@@ -330,7 +334,7 @@ int main()
 	{
 		
 		Coords C1, C2;
-		C1.X = 20; C1.Y = 20; C2.X = 780; C2.Y = 580; // 780 = (38 * 2 * 100) + 20......580 = (28 * 2 * 100) + 20
+		C1.X = 20; C1.Y = 20; C2.X = 890; C2.Y = 690; // 780 = (38 * 2 * 100) + 20......580 = (28 * 2 * 100) + 20
 		PlanoCartesiano Plano(C1, C2, CP1, CP2);
 		
 	
